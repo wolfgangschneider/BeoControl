fb5289fa20e370474c42b8261fcc8060ff096070
Wolfgang Schneider <Wolfgang@easybyte.at>
2026-03-16 21:59:51 +0100

    Update project GUIDs and reformat PC2ControlExample files
    Replaced project reference and GUID in solution file. Reformatted PC2ControlExample.csproj and Program.cs for consistency and readability. No functional changes to code.

--- UI/PC2ControlExample/Program.cs
+++ UI/PC2ControlExample/Program.cs
@@ -1,12 +1,11 @@
-﻿using BeoControl.Interfaces;
-using Pc2Adapter;
+﻿using Pc2Adapter;
 
-Console.WriteLine("=== PC2 Control Example ===");
-Console.WriteLine("Connecting to PC2...");
+//Console.WriteLine("=== PC2 Control Example ===");
+//Console.WriteLine("Connecting to PC2...");
 
 using var device = new Pc2Device();
 
-device.OnLog           += msg => Console.WriteLine($"[{msg.Level}] {msg.Text}");
+device.OnLog += msg => Console.WriteLine($"[{msg.Level}] {msg.Text}");
 device.OnStatusChanged += msg => Console.WriteLine($"  {msg.Text}");
 
 await device.Connect();
@@ -28,10 +27,10 @@ while (true)
 
     switch (input)
     {
-        case "cd":    device.SendCommand("CD");         break;
-        case "radio": device.SendCommand("RADIO");      break;
-        case "vol+":  device.SendCommand("VOL",  "1");  break;
-        case "vol-":  device.SendCommand("VOL", "-1");  break;
+        case "cd": device.SendCommand("CD"); break;
+        case "radio": device.SendCommand("RADIO"); break;
+        case "vol+": device.SendCommand("VOL", "1"); break;
+        case "vol-": device.SendCommand("VOL", "-1"); break;
         case "exit":
         case "quit":
         case "q":
